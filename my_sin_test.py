7db68eeb828ddd308302975ff4467b9c1128c471
sina87 <45138202+sina87@users.noreply.github.com>
2018-11-18 02:19:01 -0500

    Add files via upload

---
+++ my_sin_test.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 18 02:10:50 2018
+
+@author: sinaj
+"""
+
+import numpy as np
+
+print('This is a test code that calculates sin(45) :: ')
+
+angle = 45 # degrees
+
+my_sin = np.sin(np.deg2rad(angle))
+
+print (my_sin)
